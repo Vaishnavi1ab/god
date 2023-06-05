b0ab8d65bf412ed1a502ac48f9826ae501feb8bc
Vaishnavi1ab <94227105+Vaishnavi1ab@users.noreply.github.com>
2023-06-05 15:53:49 +0530

    Update Car.js

--- PRO-C151-CUSTOM-CAR-DESIGN-main/js/Car.js
+++ PRO-C151-CUSTOM-CAR-DESIGN-main/js/Car.js
@@ -11,6 +11,20 @@ AFRAME.registerComponent("car-model", {
     this.el.setAttribute("rotation", rotation);
   },
   
+    update:function(){
+    window.addEventListener("click",e=>{
+      this.data.clickCounter= this.data.clickCounter+1;
+      if(this.data.clickCounter===1){
+        const rotation={x:0,y:20,z:90};
+        this.el.setAttribute("rotation", rotation);
+
+      }else if(this.data.clickCounter===2)
+      {
+        const rotation={x:0,y:100,z:0}
+      }
+    })
+  }
+  
     
   
 });
