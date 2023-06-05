ee206117dc40e1574c7d5c88dcca3291eee6c0f0
Vaishnavi1ab <94227105+Vaishnavi1ab@users.noreply.github.com>
2023-06-05 15:53:34 +0530

    Update Base.js

--- PRO-C151-CUSTOM-CAR-DESIGN-main/js/Base.js
+++ PRO-C151-CUSTOM-CAR-DESIGN-main/js/Base.js
@@ -15,17 +15,5 @@ AFRAME.registerComponent("base", {
     });
     this.el.setAttribute("material", { color: "#1769aa" });
   },
-  update:function(){
-    window.addEventListener("click",e=>{
-      this.data.clickCounter= this.data.clickCounter+1;
-      if(this.data.clickCounter===1){
-        const rotation={x:0,y:20,z:90};
-        this.el.setAttribute("rotation", rotation);
 
-      }else if(this.data.clickCounter===2)
-      {
-        const rotation={x:0,y:100,z:0}
-      }
-    })
-  }
 });
