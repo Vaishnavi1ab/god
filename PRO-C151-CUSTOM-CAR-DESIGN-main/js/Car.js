8a03d07aec80341d3960c7aa80bb62a8fb86e3a3
Vaishnavi1ab <94227105+Vaishnavi1ab@users.noreply.github.com>
2023-06-05 15:58:41 +0530

    Update Car.js

--- PRO-C151-CUSTOM-CAR-DESIGN-main/js/Car.js
+++ PRO-C151-CUSTOM-CAR-DESIGN-main/js/Car.js
@@ -6,7 +6,7 @@ AFRAME.registerComponent("car-model", {
     // Do something when component first attached.
     this.el.setAttribute("gltf-model", this.data.modelRef);
     const position = { x: 0, y: 50, z: 80 };
-    const rotation = { x: 50, y: -100, z: 0 };
+    const rotation = { x: 0, y: -100, z: 0 };
     this.el.setAttribute("position", position);
     this.el.setAttribute("rotation", rotation);
   },
